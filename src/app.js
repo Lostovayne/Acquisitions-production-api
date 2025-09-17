eaba07fcbdb75116082f2da43edee803221c0369
LostoVayne <moises.barrera.dev@gmail.com>
2025-09-17 20:24:04 -0300

    feat: implement logging with Winston and add middleware for cookie parsing, CORS, and request logging

--- src/app.js
+++ src/app.js
@@ -1,8 +1,26 @@
+import logger from '#config/logger';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
 import express from 'express';
+import helmet from 'helmet';
+import morgan from 'morgan';
 
 const app = express();
 
+// Middleware setup
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(
+  morgan('combined', {
+    stream: { write: message => logger.info(message.trim()) },
+  })
+);
+
 app.get('/', (_req, res) => {
+  logger.info('Root endpoint accessed');
   res.status(200).send('Hello, World!');
 });
 
